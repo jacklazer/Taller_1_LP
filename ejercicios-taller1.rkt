92ebd0bdbf2f2c9d9ac7927c81b5082e9e395047
Juan CV <jacklazerjscv@gmail.com>
2023-09-10 13:58:51 -0500

    funcion 7

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -34,6 +34,7 @@
 (newline)
 (newline)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; 4)
 ;; filter-in:
@@ -64,3 +65,74 @@
 (display (filter-in symbol? '(a (b c) 17 foo)))
 (newline)
 (newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 7)
+;; cartesian-product-aux:
+;; Proposito:
+;; L1 x L2 x L3 -> L’ : Procedimiento que recibe tres listas, L1, L2 y L3,
+;; y devuelve una lista L' con el producto cartesiano entre L1 y L2
+;; siempre y cuando L2 sea igual a L3.
+;;
+;; <lista-de-datos> := ({<tipo-de-dato>}*)
+;;
+;; <lista-de-tuplas> := ({<tupla-de-datos>}*)
+;;
+;; <tupla-de-datos> := (<tipo-de-dato> <tipo-de-dato>)
+;;
+;; <tipo-de-dato> := <int> | <simbolo> | <otro-tipo-de-dato>
+
+(define cartesian-product-aux
+  (lambda (L1 L2 L3)
+    (cond
+      [(null? L1) L1]
+      [(and (not (null? L1)) (not (null? L3))) (cons (cons (car L1) (cons (car L3) '())) (cartesian-product-aux L1 L2 (cdr L3)))]
+      [(null? L3) (cartesian-product-aux (cdr L1) L2 L2)]
+    )
+  )
+)
+
+
+;; cartesian-product:
+;; Proposito:
+;; L1 x L2 -> L’ : Procedimiento que recibe dos listas, L1 y L2,
+;; y devuelve una lista L' con el producto cartesiano entre L1 y L2.
+;;
+;; <lista-de-datos> := ({<tipo-de-dato>}*)
+;;
+;; <lista-de-tuplas> := ({<tupla-de-datos>}*)
+;;
+;; <tupla-de-datos> := (<tipo-de-dato> <tipo-de-dato>)
+;;
+;; <tipo-de-dato> := <int> | <simbolo> | <otro-tipo-de-dato>
+
+(define cartesian-product
+  (lambda (L1 L2)
+    (cartesian-product-aux L1 L2 L2)
+  )
+)
+
+;; Pruebas
+(display (cartesian-product '(a b c) '(x y)))
+(newline)
+(display (cartesian-product '(p q r) '(5 6 7)))
+(newline)
+(newline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 7)
+;; cartesian-product-aux:
+;; Proposito:
+;; L1 x L2 x L3 -> L’ : Procedimiento que recibe tres listas, L1, L2 y L3,
+;; y devuelve una lista L' con el producto cartesiano entre L1 y L2
+;; siempre y cuando L2 sea igual a L3.
+;;
+;; <lista-de-datos> := ({<tipo-de-dato>}*)
+;;
+;; <lista-de-tuplas> := ({<tupla-de-datos>}*)
+;;
+;; <tupla-de-datos> := (<tipo-de-dato> <tipo-de-dato>)
+;;
+;; <tipo-de-dato> := <int> | <simbolo> | <otro-tipo-de-dato>
